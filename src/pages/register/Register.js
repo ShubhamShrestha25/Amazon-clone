dd470a392b4dadb8d20c4b6e90be5f55a43e2aa5
Shubham Shrestha <shubhamshrestha25@gmail.com>
2022-01-31 20:43:16 +0545

    register and login page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import AmazonLogo from "../../Amazon_Logo.png";
 import "./Register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="register">
+      <Link to="/">
+        <img src={AmazonLogo} alt="" className="register-logo" />
+      </Link>
+      <div className="register-container">
+        <h1>Create Account</h1>
+        <form>
+          <h5>E-Mail</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={register} className="continue">
+            Continue
+          </button>
+          <div className="detail">
+            <p>Already have an account ?</p>
+            <Link to="/login" className="signin-link">
+              <p>Sign In</p>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
