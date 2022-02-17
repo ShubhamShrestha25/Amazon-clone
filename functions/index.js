ba9bc9e301eefdf5d06ff7b4887ba7157160de06
Shubham Shrestha <shubhamshrestha25@gmail.com>
2022-02-17 16:58:40 +0545

    updated

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,9 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
-const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51J69r5IDhtOyKcx9z1gzsR4VZ6sZc2YIIuOvz9kYghyn4VMFecV1yn6zITtYWd0vufoIWGwYuNqQQ0xiG7Rq083100suByfBM9"
+);
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -11,14 +13,6 @@ app.use(express.json());
 app.get("/", (request, response) =>
   response.status(200).send("Hello from Cloud")
 );
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 app.post("/payments/create", async (req, res) => {
   const total = req.query.total;
